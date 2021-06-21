b0c52f45da3ee4cc355751b903696141989548e0
gugli <guglielmojonathan@gmail.com>
2021-06-21 10:38:58 -0300

    agregando animaciones con ScrollReveal

--- js/main.js
+++ js/main.js
@@ -4,3 +4,43 @@ btnSwitch.addEventListener('click' , () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
 });
+
+window.sr = ScrollReveal();
+    sr.reveal ('.navbar', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-300px'
+    });
+
+window.sr = ScrollReveal();
+    sr.reveal ('.dg', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-300px'
+    });
+
+window.sr = ScrollReveal();
+    sr.reveal ('.dm', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-300px'
+    });
+
+window.sr = ScrollReveal();
+    sr.reveal ('.dw', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-300px',
+    });
+
+window.sr = ScrollReveal();
+    sr.reveal ('.cm', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-300px',
+    });
+
+window.sr = ScrollReveal();
+    sr.reveal ('.disenog', {
+        delay: 500,
+    });
