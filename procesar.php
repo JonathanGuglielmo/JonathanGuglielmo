4c71af56c3cb7df59b96f28648e01aa2fbfeb746
gugli <guglielmojonathan@gmail.com>
2021-06-23 21:19:26 -0300

    modificando PHP

--- procesar.php
+++ procesar.php
@@ -1,13 +1,19 @@
 <?php
+
+if (isset($_POST['enviar'])) {
+    if (!empty($_POST['name']) && !empty($_POST['asunto']) && !empty($_POST['msg']) && !empty($_POST[email])){ 
 $nombre = $_POST ['introducir_nombre'];
 $email = $_POST ['introducir_email'];
 $telefono = $_POST ['introducir_telefono'];
 $mensaje = $_POST ['introducir_mensaje'];
-
-echo "<h2> Información recibida desde PHP </h2>";
-echo "el nombre recibido es:" . $nombre;
-echo "el mail recibido es:" . $email;
-echo "el telefono recibido es:" . $telefono;
-echo "el mensaje recibido es:" . $mensaje;
+$header = "From: noreply@example.com" . "\r\n";
+$header.= "Reply-To: noreply@example.com" . "\r\n";
+$header.= "X-Mailer: PHP/" . phpversion ();
+mail = @mail($email,$asunto,$msg,$header);
+if ($mail) {
+    echo "<h4>¡Mail enviado exitosamente!</h4>";
+}
+}
+}
 
 ?>
